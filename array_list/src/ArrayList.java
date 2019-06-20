71edaeb65ba7bcb21f0d7966010dfd029c8f2556
Naoto Abe <naotoabe1@gmail.com>
2019-06-19 23:10:56 -0400

    setup array_list interface and arrayList class

---
+++ array_list/src/ArrayList.java
@@ -0,0 +1,46 @@
+public class ArrayList<T> implements ArrayListInterface<T> {
+
+    public void addAtIndex(int index, T data) {
+
+    }
+
+    public void addToFront(T data) {
+
+    }
+
+    public void addToBack(T data) {
+
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T removeAtIndex(int index) {
+        return null;
+    }
+
+    public T removeFromFront() {
+        return null;
+    }
+
+    public T removeFromBack() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public Object[] getBackingArray() {
+        return null;
+    }
+}
